2c22a542550f97aa3c3caa271ad9287fccf80bd8
khyathivagolu <khyathi@iitk.ac.in>
2021-07-12 00:47:16 +0530

    implemented breaking out of the loop

--- Foraging task/foraging basic env 1/untitled_lastrun.py
+++ Foraging task/foraging basic env 1/untitled_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on Tue 06 Jul 2021 09:44:00 PM IST
+    on Wed 07 Jul 2021 04:39:55 PM IST
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -51,7 +51,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='/home/khyathi/Desktop/surge/Foraging task/foraging basic env 1/untitled_lastrun.py',
+    originPath='/home/khyathi/Desktop/surge/git uploads/Foraging task/foraging basic env 1/untitled_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
